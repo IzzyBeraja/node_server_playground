d5f7ea2090df26528f06ea40ab0f435ed7ed61be
IzzyBeraja <izzyberaja+github@gmail.com>
2020-10-24 18:51:29 -0400

    Mongoose connect and ApolloServer 🐱‍🏍

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,20 @@
-import { ApolloServer, gql } from "apollo-server-express";
+require("dotenv").config();
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { schema } from "./schema/schema";
+import mongoose from "mongoose";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 
 const app = express();
 
-const typeDefs = gql`
-  type Query {
-    hello: String!
-  }
-`;
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cpets.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`
+);
 
-//app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const port = 4000;
 app.listen(port, () => {
