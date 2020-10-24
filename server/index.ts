732e1cdddbc62e64100d170228a6cd8df7190bb0
IzzyBeraja <izzyberaja+github@gmail.com>
2020-10-24 18:29:57 -0400

    Added Apollo, and mongoose 🚀

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,16 @@
+import { ApolloServer, gql } from "apollo-server-express";
 import express from "express";
-import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema/schema";
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+//app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 const port = 4000;
 app.listen(port, () => {
