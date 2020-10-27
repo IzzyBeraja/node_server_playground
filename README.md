17b4048ce55ac36466a19da03497bacfeaff58f4
IzzyBeraja <izzyberaja+github@gmail.com>
2020-10-27 14:17:17 -0400

    Added ApolloServer and Readme 🚀📖

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+This is a node server playground. I'm currently implementing Mikro-ORM with a
+MongoDB backend that communicates over GraphQL. All of this project will be
+written in TypeScript and currently does not have a working front end as of yet.
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
 import { Post } from "./entities/Post";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const startServer = async () => {
   const { em } = await MikroORM.init(mikroOrmConfig);
@@ -9,11 +12,17 @@ const startServer = async () => {
   const app = express();
   //   const post = orm.em.create(Post, { title: "my first post", random: 19 });
   //   await orm.em.persistAndFlush(post);
-  const posts = await em.find(Post, {});
 
-  console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
 
-  const port = 5234;
+  apolloServer.applyMiddleware({ app });
+
+  const port = 4000;
   app.listen({ port }, () => {
     console.info(`\n😁 Server is now ready at http://localhost:${port} 👍🏻\n`);
   });
